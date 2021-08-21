7a7fac247e05e5f864aa5a11ddb26330cdc76ab2
AmoghAgarwal1600 <81306791+AmoghAgarwal1600@users.noreply.github.com>
2021-08-21 16:51:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("this is my first console project");
+var first_name="Amogh";
+var last_name="Agarwal";
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+var a= 100;
+var b= 200;
+var add=a+b;
+console.log(add);
+var sub=a-b;
+console.log(sub);
+var mul=a*b;
+console.log(mul);
+var div=a/b;
+console.log(div);
